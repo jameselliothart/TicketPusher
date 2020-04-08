9ea6894a332ed06183e24c3b377c73c75b94e7b8
James Hart <james.elliot.hart@gmail.com>
2020-04-08 16:38:16 -0500

    create db context

---
+++ TicketPusher.API/Data/TicketPusherContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TicketPusher.Domain.Tickets;
+
+namespace TicketPusher.API.Data
+{
+    public sealed class TicketPusherContext : DbContext
+    {
+        public TicketPusherContext(DbContextOptions<TicketPusherContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>(x =>
+            {
+                x.ToTable("Ticket").HasKey(k => k.Id);
+                x.Property(p => p.Id).HasColumnName("TicketId");
+            });
+        }
+    }
+}
