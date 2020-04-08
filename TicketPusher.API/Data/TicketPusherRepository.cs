5d4c4af9b9287e050d57473bd3c589998e8adb1b
James Hart <james.elliot.hart@gmail.com>
2020-04-08 16:38:48 -0500

    create repository

---
+++ TicketPusher.API/Data/TicketPusherRepository.cs
@@ -0,0 +1,30 @@
+using System;
+using TicketPusher.Domain.Tickets;
+
+namespace TicketPusher.API.Data
+{
+    public class TicketPusherRepository
+    {
+        private readonly TicketPusherContext _context;
+
+        public TicketPusherRepository(TicketPusherContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void CreateTicket(Ticket ticket)
+        {
+            if (ticket.Id == Guid.Empty) 
+                throw new ArgumentException($"Ticket Id {ticket.Id} cannot be empty");
+            if (ticket == null)
+                throw new ArgumentNullException(nameof(ticket));
+
+            _context.Tickets.Add(ticket);
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
