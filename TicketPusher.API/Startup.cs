bc6dfe863faa9f3b9fac9ae85f1f2be2a9544518
James Hart <james.elliot.hart@gmail.com>
2020-04-08 16:37:24 -0500

    add ef core with postgresql

--- TicketPusher.API/Startup.cs
+++ TicketPusher.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TicketPusher.API.Data;
 
 namespace TicketPusher.API
 {
@@ -26,6 +28,11 @@ namespace TicketPusher.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<TicketPusherContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("TicketPusherDb"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
