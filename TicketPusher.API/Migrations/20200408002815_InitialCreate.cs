1508ec9af4f7f4481cb2aa676b551f63ef3106c7
James Hart <james.elliot.hart@gmail.com>
2020-04-08 16:39:12 -0500

    add initial migration

---
+++ TicketPusher.API/Migrations/20200408002815_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketPusher.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    TicketId = table.Column<Guid>(nullable: false),
+                    Owner = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    SubmitDate = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.TicketId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket");
+        }
+    }
+}
