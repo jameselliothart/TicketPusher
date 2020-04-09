29a714e7e951fa184e000949902798a71eca5c1c
James Hart <james.elliot.hart@gmail.com>
2020-04-09 18:37:39 -0500

    add MediatR

--- TicketPusher.API/Startup.cs
+++ TicketPusher.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,6 +36,7 @@ namespace TicketPusher.API
             });
 
             services.AddScoped<ITicketPusherRepository, TicketPusherRepository>();
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
