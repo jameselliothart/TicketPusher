0803ed8f9b8991fe1f22e04c3844c37771cef68f
James Hart <james.elliot.hart@gmail.com>
2020-04-13 20:20:05 -0500

    create ticket controller

---
+++ TicketPusher.API/Tickets/TicketController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TicketPusher.API.Tickets.Commands;
+using TicketPusher.API.Tickets.Queries;
+using TicketPusher.Domain.Tickets;
+
+namespace TicketPusher.API.Tickets
+{
+    [ApiController]
+    [Route("api/tickets")]
+    public sealed class TicketController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TicketController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TicketDto>>> GetTicketList()
+        {
+            var query = new GetTicketListQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TicketDto>> GetTicket(Guid id)
+        {
+            var query = new GetTicketQuery(id);
+            var result = await _mediator.Send(query);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TicketDto>> CreateTicket([FromBody] SubmitTicketDto ticket)
+        {
+            var dueDate = ticket.DueDate ?? NoSetDate.Instance;
+            var command = new SubmitTicketCommand(ticket.Owner, ticket.Description, dueDate);
+            var result = await _mediator.Send(command);
+            
+            return CreatedAtAction(nameof(GetTicket),
+                new { id = result.Id },
+                result);
+        }
+    }
+}
