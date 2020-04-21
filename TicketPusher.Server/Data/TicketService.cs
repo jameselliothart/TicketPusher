31908e0c158853a6f3c9169746e17a462712133f
James Hart <james.elliot.hart@gmail.com>
2020-04-21 18:48:46 -0500

    show example tickets in UI

--- TicketPusher.Server/Data/TicketService.cs
+++ TicketPusher.Server/Data/TicketService.cs
@@ -2,14 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketPusher.API.Tickets;
 
 namespace TicketPusher.Server.Data
 {
     public class TicketService
     {
-        private Ticket[] tickets = {new Ticket("James", "first ticket", DateTime.Now), new Ticket("Jennifer", "second ticket", DateTime.Now)};
-        public Task<Ticket[]> GetTicketsAsync()
+        public Task<IEnumerable<TicketDto>> GetTicketsAsync()
         {
+            var tickets = Enumerable.Range(0, 3).Select(i => 
+            {
+                return new TicketDto
+                {
+                    Id = Guid.NewGuid(),
+                    Owner = "Unassigned",
+                    Description = $"Ticket {i}",
+                    SubmitDate = DateTime.Now,
+                    DueDate = DateTime.Now.AddDays(i),
+                    ProjectId = Guid.NewGuid()
+                };
+            });
             return Task.FromResult(tickets);
         }
     }
--- TicketPusher.Server/Pages/TicketsBase.cs
+++ TicketPusher.Server/Pages/TicketsBase.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketPusher.API.Tickets;
 using TicketPusher.Server.Data;
 
 namespace TicketPusher.Server.Pages
@@ -10,11 +11,14 @@ namespace TicketPusher.Server.Pages
     public class TicketsBase : ComponentBase
     {
         [Inject] private TicketService ticketService { get; set; }
-        public Ticket[] tickets;
+
+        private List<TicketDto> _tickets = new List<TicketDto>();
+        public IReadOnlyList<TicketDto> tickets => _tickets.ToList();
 
         protected override async Task OnInitializedAsync()
         {
-            tickets = await ticketService.GetTicketsAsync();
+            var data = await ticketService.GetTicketsAsync();
+            _tickets.AddRange(data);
         }
     }
 }
