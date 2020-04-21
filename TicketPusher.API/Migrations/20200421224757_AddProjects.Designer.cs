2dd4dc9cbdce4badc8a0876bb9e3e5b264ad0077
James Hart <james.elliot.hart@gmail.com>
2020-04-21 17:49:36 -0500

    add migration for projects table

--- TicketPusher.API/Migrations/20200421224757_AddProjects.Designer.cs
+++ TicketPusher.API/Migrations/20200421224757_AddProjects.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TicketPusher.API.Data;
@@ -9,9 +10,10 @@ using TicketPusher.API.Data;
 namespace TicketPusher.API.Migrations
 {
     [DbContext(typeof(TicketPusherContext))]
-    partial class TicketPusherContextModelSnapshot : ModelSnapshot
+    [Migration("20200421224757_AddProjects")]
+    partial class AddProjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
