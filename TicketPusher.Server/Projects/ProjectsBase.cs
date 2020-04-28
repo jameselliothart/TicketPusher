f7f7cc2bdd7565e4f5f09397b1218dfe56fbfe30
James Hart <james.elliot.hart@gmail.com>
2020-04-28 18:04:21 -0500

    first pass at ability to add projects

--- TicketPusher.Server/Projects/ProjectsBase.cs
+++ TicketPusher.Server/Projects/ProjectsBase.cs
@@ -11,25 +11,24 @@ namespace TicketPusher.Server.Projects
     public class ProjectsBase : ComponentBase
     {
         [Inject]
-        private IProjectDataService projectDataService { get; set; }
+        protected IProjectDataService projectDataService { get; set; }
         [Inject]
-        private IToastService _toastService { get; set; }
+        protected IToastService _toastService { get; set; }
 
-        private List<ProjectDto> _projects = new List<ProjectDto>();
-        public IReadOnlyList<ProjectDto> projects => _projects.ToList();
+        // private List<ProjectDto> _projects = new List<ProjectDto>();
+        public List<ProjectDto> projects;
 
         protected CreateProjectDto ProjectModel = new CreateProjectDto();
 
-        protected async void HandleValidSubmit()
-        {
-            var data = await projectDataService.CreateProjectAsync(ProjectModel);
-            _toastService.ShowSuccess($"Added project {data.Result.Id}", "Success!");
-        }
-
         protected override async Task OnInitializedAsync()
         {
+            await RefreshData();
+        }
+
+        protected async Task RefreshData()
+        {
             var data = await projectDataService.GetProjectListAsync();
-            _projects.AddRange(data.Result);
+            projects = data.Result;
         }
     }
 }
