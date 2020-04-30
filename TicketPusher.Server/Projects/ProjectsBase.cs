7b8465be8e61a397023fb66d0ee2ba27e8bfc853
James Hart <james.elliot.hart@gmail.com>
2020-04-29 20:09:48 -0500

    show modal to add project

--- TicketPusher.Server/Projects/ProjectsBase.cs
+++ TicketPusher.Server/Projects/ProjectsBase.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Modal.Services;
 using Blazored.Toast.Services;
 using Microsoft.AspNetCore.Components;
 using TicketPusher.API.Projects;
@@ -11,10 +12,18 @@ namespace TicketPusher.Server.Projects
 {
     public class ProjectsBase : EntityBase<ProjectDto, IProjectDataService>
     {
+        [Inject]
+        private IModalService _modal { get; set; }
+
         protected override async Task<List<ProjectDto>> RefreshData()
         {
             var data = await EntityDataService.GetProjectListAsync();
             return data.Result;
+        }
+
+        public void AddProject()
+        {
+            _modal.Show(typeof(EditProject), "Add Project");
         }
 
     }
