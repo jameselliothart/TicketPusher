4be66f7dd286a0f75c362dc391a75821f09e7003
James Hart <james.elliot.hart@gmail.com>
2020-06-24 23:21:04 -0500

    test request handler instead of controller

---
+++ TicketPusher.API.Tests/CompletedTickets/GetCompletedTicketListQueryHandlerShould.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using CSharpFunctionalExtensions;
+using FluentAssertions;
+using TicketPusher.API.CompletedTickets;
+using TicketPusher.API.CompletedTickets.Queries;
+using TicketPusher.API.Tests.Utils;
+using TicketPusher.API.Utils;
+using TicketPusher.Domain.Tests.Utils;
+using Xunit;
+
+namespace TicketPusher.API.Tests.CompletedTickets
+{
+    public class GetCompletedTicketListQueryHandlerShould : RequestHandlerShouldSetup, IClassFixture<MapperFixture>
+    {
+        public GetCompletedTicketListQueryHandlerShould(MapperFixture mapper) : base(mapper)
+        {
+        }
+
+        [Fact]
+        public void ReturnCompletedTickets()
+        {
+            // Arrange
+            var query = new GetCompletedTicketListQuery();
+            var completedTickets = TicketTestData.TicketList().Select(t => t.Close("Done.")).ToList();
+
+            ActWithContext(async ctx =>
+            {
+                ctx.CompletedTickets.AddRange(completedTickets);
+                await ctx.SaveChangesAsync();
+            });
+
+            ActWithRepository(async repo =>
+            {
+                // Act
+                var queryHandler = new GetCompletedTicketListQueryHandler(repo, _mapper.Instance);
+                Result<IEnumerable<CompletedTicketDto>> result = await queryHandler.Handle(query, new CancellationToken());
+
+                // Assert
+                result.Value.Select(t => t.Id).Should().BeEquivalentTo(completedTickets.Select(t => t.Id));
+            });
+        }
+    }
+}
