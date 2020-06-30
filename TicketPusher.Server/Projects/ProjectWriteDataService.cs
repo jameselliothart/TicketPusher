378dc6c2630a1e6532bb66141e6b2d2079726ee3
James Hart <james.elliot.hart@gmail.com>
2020-06-30 18:43:24 -0500

    update project with PUT

--- TicketPusher.Server/Projects/ProjectWriteDataService.cs
+++ TicketPusher.Server/Projects/ProjectWriteDataService.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using TicketPusher.DataTransfer.Projects;
+using TicketPusher.Server.Shared;
 using TicketPusher.Server.Templates;
 
 namespace TicketPusher.Server.Projects
@@ -9,6 +13,12 @@ namespace TicketPusher.Server.Projects
 
         public ProjectWriteDataService(HttpClient httpClient) : base(httpClient, "projects")
         {
+        }
+
+        public async Task<EnvelopeDto<ProjectDto>> UpdateProjectAsync(Guid projectId, UpdateProjectDto project)
+        {
+            var data = await HttpClient.PutJsonAsync<EnvelopeDto<ProjectDto>>($"{ApiBaseUri}/{projectId}", project);
+            return data;
         }
 
     }
