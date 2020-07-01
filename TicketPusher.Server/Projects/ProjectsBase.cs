c2a2a7a5c1b073ef0f1ccc220e96e16c1ee2deff
James Hart <james.elliot.hart@gmail.com>
2020-07-01 06:42:18 -0500

    do not display None project in table

--- TicketPusher.Server/Projects/ProjectsBase.cs
+++ TicketPusher.Server/Projects/ProjectsBase.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TicketPusher.DataTransfer.Projects;
+using TicketPusher.Domain.Projects;
 using TicketPusher.Server.Templates;
 
 namespace TicketPusher.Server.Projects
 {
     public class ProjectsBase : EntityBase<ProjectDto, IProjectReadDataService>
     {
+        public IEnumerable<ProjectDto> EditableProjects =>
+            Entities?.Where(p => p.Id != Project.None.Id);
 
         protected override async Task RefreshData()
         {
