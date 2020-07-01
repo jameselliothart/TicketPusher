32b2403120ce6ba2a54b094e5a4f9f7128be57bd
James Hart <james.elliot.hart@gmail.com>
2020-07-01 06:39:14 -0500

    add method to interface

--- TicketPusher.Server/Projects/IProjectWriteDataService.cs
+++ TicketPusher.Server/Projects/IProjectWriteDataService.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Threading.Tasks;
 using TicketPusher.DataTransfer.Projects;
+using TicketPusher.Server.Shared;
 using TicketPusher.Server.Templates;
 
 namespace TicketPusher.Server.Projects
 {
     public interface IProjectWriteDataService : IEntityWriteDataService<ProjectDto, CreateProjectDto>
     {
+        Task<EnvelopeDto<ProjectDto>> UpdateProjectAsync(Guid projectId, UpdateProjectDto project);
     }
 }
